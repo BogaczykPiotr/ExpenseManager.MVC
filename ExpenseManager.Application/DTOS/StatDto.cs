7d24cb5242f9ecb0c0f9a7576991d6b830d59835
Piotr Bogaczyk <piotr.bogaczyk@icloud.com>
2023-08-04 19:15:17 +0200

    Remove unnecessary records

--- ExpenseManager.Application/DTOS/StatDto.cs
+++ ExpenseManager.Application/DTOS/StatDto.cs
@@ -12,8 +12,6 @@ namespace ExpenseManager.Application.DTOS
         public float TotalAmount { get; set; }
         public float Spent { get; set; }
         public float Left { get; set; }
-        public int UserForeignKey { get; set; }
-        public int SavingGoalForeignKey { get; set; }
         public SavingGoal SavingGoal { get; set; }
     }
 }
