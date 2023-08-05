b928a20c45ce550e3719a312abb6d7d40add2e70
Piotr Bogaczyk <piotr.bogaczyk@icloud.com>
2023-08-05 13:33:10 +0200

    Add stat query

--- ExpenseManager.Application/Queries/GetStatValues/GetStatValuesQuery.cs
+++ ExpenseManager.Application/Queries/GetStatValues/GetStatValuesQuery.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ExpenseManager.Application.DTOS;
+using ExpenseManager.Domain.Interfaces;
+using MediatR;
 
 namespace ExpenseManager.Application.Queries.GetStatValues
 {
-    internal class GetStatValuesQuery
+    public class GetStatValuesQuery : IRequest<StatDto>
     {
     }
 }
+
