4808a1d4ba98e9801a7e6c1cc5e2d8264b00c69d
Piotr Bogaczyk <139002468+BogaczykPiotr@users.noreply.github.com>
2023-08-11 14:43:09 +0200

    Add Id to SavingGoalDto

--- ExpenseManager.Application/DTOS/SavingGoalDto.cs
+++ ExpenseManager.Application/DTOS/SavingGoalDto.cs
@@ -8,6 +8,7 @@ namespace ExpenseManager.Application.DTOS
 {
     public class SavingGoalDto
     {
+        public int Id { get; set; } 
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public int Goal { get; set; }
     }
