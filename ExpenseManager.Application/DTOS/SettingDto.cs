2238f1a79c5775fa4463c0a167a7d1bd5c42d5b7
Piotr Bogaczyk <139002468+BogaczykPiotr@users.noreply.github.com>
2023-08-13 20:05:34 +0200

    Create SettingDto.cs

---
+++ ExpenseManager.Application/DTOS/SettingDto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseManager.Application.DTOS
+{
+    public class SettingDto
+    {
+        public string Currency { get; set; }
+        public string Language { get; set; }
+    }
+}
