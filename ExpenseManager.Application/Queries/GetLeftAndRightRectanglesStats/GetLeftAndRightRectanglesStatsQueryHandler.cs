f0dcfbe6e83137fb10074a5f4763bac599b6ae16
Piotr Bogaczyk <piotr.bogaczyk@icloud.com>
2023-08-31 22:31:53 +0200

    Add query for all views (language, currency, actions)

---
+++ ExpenseManager.Application/Queries/GetLeftAndRightRectanglesStats/GetLeftAndRightRectanglesStatsQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseManager.Application.Queries.GetLeftAndRightRectanglesStats
+{
+    internal class GetLeftAndRightRectanglesStatsQueryHandler
+    {
+    }
+}
