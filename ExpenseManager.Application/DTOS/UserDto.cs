61cdc0ee2a2678fc709964cad2cedbe5173872ba
Piotr Bogaczyk <piotr.bogaczyk@icloud.com>
2023-11-19 21:56:45 +0100

    Add user table

---
+++ ExpenseManager.Application/DTOS/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ExpenseManager.Application.DTOS
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public string? Username { get; set; }
+
+    }
+}
---
+++ ExpenseManager.Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseManager.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [MaxLength(25)]
+        public string? Name { get; set; }
+        [MaxLength(25)]
+        public string? LastName { get; set; }
+        [MaxLength(25)]
+        public string? Username { get; set; }
+        
+    }
+}
