58f8bc993c290e8ce54a26148e5f6ba2b0f1d5b5
Piotr Bogaczyk <piotr.bogaczyk@icloud.com>
2023-12-18 20:45:12 +0100

    Customize login and register pages

--- ExpenseManager.MVC/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ ExpenseManager.MVC/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -115,7 +115,7 @@ namespace ExpenseManager.MVC.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
-                    return LocalRedirect(returnUrl);
+                    return RedirectToAction("Dashboard", "ExpenseManager");
                 }
                 if (result.RequiresTwoFactor)
                 {
