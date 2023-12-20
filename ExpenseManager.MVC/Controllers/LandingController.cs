f6d71abc45f946a1ae78a79ddd492613875ce3f6
Piotr Bogaczyk <piotr.bogaczyk@icloud.com>
2023-12-20 23:01:40 +0100

    Change index in landing controller

--- ExpenseManager.MVC/Controllers/LandingController.cs
+++ ExpenseManager.MVC/Controllers/LandingController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ExpenseManager.MVC.Controllers
@@ -6,8 +7,18 @@ namespace ExpenseManager.MVC.Controllers
     [AllowAnonymous]
     public class LandingController : Controller
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public LandingController(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
         public async Task<IActionResult> Index()
         {
+            if (_signInManager.IsSignedIn(User))
+            {
+                return RedirectToAction("Dashboard", "ExpenseManager");
+            }
+
             return View();
         }
     }
